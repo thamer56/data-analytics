3b980a88881901c8e06662f26f66005c453f39fa
thamer56 <thamerbelghalmi611@gmail.com>
2024-04-12 14:59:58 +0100

    Count

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // import data 
 const dataArray = require("./sData.js");
+const counts = dataArray.reduce((acc, obj) => {
+    acc[obj.status] = (acc[obj.status] || 0) + 1;
+    return acc;
+  }, {});
+  
+  const result = Object.keys(counts).map(status => ({ status, count: counts[status] }));
+  
+  console.log(result);
