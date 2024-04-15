b4466868be456566877de6924f05684c457e6a72
thamer56 <thamerbelghalmi611@gmail.com>
2024-04-15 14:01:21 +0100

    Update

---
+++ functions.js
@@ -0,0 +1,39 @@
+function GroupbyStatus(dataArray){
+    const counts = dataArray.reduce((acc, obj) => {
+        acc[obj.status] = (acc[obj.status] || { count: 0, totalResponses: 0 });
+        acc[obj.status].count += 1;
+        acc[obj.status].totalResponses += obj.numberOfResponses;
+        return acc;
+    }, {});
+
+    // Assurez-vous que toutes les valeurs possibles sont incluses
+    ['Closed', 'Active', 'Hold'].forEach(status => {
+        if (!(status in counts)) {
+            counts[status] = { count: 0, totalResponses: 0 };
+        }
+    });
+
+    return counts;
+}
+
+
+
+
+ function calculateParticipantsByMonth(dataArray) {
+    const participantsByMonth = {};
+  
+    dataArray.forEach(survey => {
+      const createdDate = new Date(survey.createdDate * 1000);
+      const monthYear = createdDate.toLocaleString('default', { month: 'short', year: 'numeric' });
+  
+      if (participantsByMonth[monthYear]) {
+        participantsByMonth[monthYear] += survey.numberOfResponses;
+      } else {
+        participantsByMonth[monthYear] = survey.numberOfResponses;
+      }
+    });
+  
+    return participantsByMonth;
+  }
+  module.exports = { GroupbyStatus, calculateParticipantsByMonth };
+  
